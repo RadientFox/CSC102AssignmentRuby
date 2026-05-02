64a88794fc8b7fa9e03b0d7cef8f67a4665e4f9d
Radient Fox <147359033+RadientFox@users.noreply.github.com>
2026-05-02 12:39:47 -0700

    Add files via upload
    6.1

--- js_script.js
+++ js_script.js
@@ -27,7 +27,7 @@ function playDiceGame(){
 }
 
 function getRandomNumber(){
-    let number = Math.random() *6;
+    let number = Math.random() + 6;
 
     number = Math.floor(number) +1;
 
@@ -36,3 +36,56 @@ function getRandomNumber(){
 
     return number;
 }
+
+    // function validates input based on client (assignment) requirements
+ function validate(){
+            //first name var
+            let firstName = document.getElementById("txtFirstName").value;
+
+            console.log("firstName= " + firstName);
+
+
+            
+            //last name var
+            let lastName = document.getElementById("txtLastName").value;
+
+            console.log("lastName= " + lastName);
+
+
+
+              
+            //zip code var
+            let zip = document.getElementById("txtZip").value;
+
+            console.log("zip= " + zip);
+
+
+            // var to hold first name + " " + last name
+
+            let fullName = firstName + " " + lastName;
+
+            console.log("fullName=" + fullName);
+
+
+            // var to hold message to show user
+            let message = "";
+
+            // full name does not go past 20 chatachters
+            if (fullName.length > 20 || fullName.length <= 1){
+
+                message = "Enter a name less then 20 Characters";
+            }
+            // zip code to only have 5 characters
+            else if (zip.length != 5){
+
+                message = "Enter a 5 difit zip code";
+            }
+            // when everything is stated corectly
+            else{
+                message = "secret word is validation!";
+            }
+
+            console.log("message= " + message);
+
+            document.getElementById("divMessage").textContent = message;
+        }
